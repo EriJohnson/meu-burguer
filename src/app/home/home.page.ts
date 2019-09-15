9213b25f50ba4d8699b3ca83e8ae8f465f1a123b
EriJohnson <erijohnsons.fernandes@gmail.com>
2019-09-14 23:06:02 -0300

    Adicionr Navegação P/ a Página de Realizar Pedidos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { PedidoPage } from '../pedido/pedido.page';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  fazerPedido(){
+    this.navCtrl.push(PedidoPage);
+  }
 
 }
